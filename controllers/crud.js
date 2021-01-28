a89d68742cd4e77d4d03a7a099ef5831c2f5db96
PedroUrtecho90 <75145945+PedroUrtecho90@users.noreply.github.com>
2021-01-27 20:51:26 -0600

    27-01-21 CRUDS
    Cambios abc de usuarios y juegos

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,17 +1,47 @@
-    const conexion =  require('../conexion/conexion');
-    const { use } = require('../route');
+const conexion =  require('../conexion/conexion');
 
-    exports.save = (req, res)=>{
-        const nombre = req.body.nombre;
-        const correo = req.body.correo;
-        const contra = req.body.contra;
-        const id_perfil = req.body.id_perfil;
-        conexion.query('INSERT INTO tblusuarios SET?',{nombre:nombre, correo:correo, contra:contra, id_perfil:id_perfil}, (error,results)=>{
-            if(error){
-                console.log(error);
-            }else{
-                res.redirect('/');
-            }
-        } )
-    }
+exports.save = (req, res)=>{
+    const nombre = req.body.nombre;
+    const correo = req.body.correo;
+    const contra = req.body.contra;
+    const id_perfil = req.body.id_perfil;
+    conexion.query('INSERT INTO tblusuarios SET?',{nombre:nombre, correo:correo, contra:contra, id_perfil:id_perfil}, (error,results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect('/');
+        }
+    } )
+};
+
+exports.save_juego = (req, res)=>{
+    const titulo = req.body.titulo;
+    const imagen = req.body.imagen;
+    const descripcion = req.body.descripcion;
+    const status = req.body.status;
+    conexion.query('INSERT INTO tbljuegos SET?',{titulo:titulo, imagen:imagen, descripcion:descripcion, status:status}, (error,results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect('/mygames');
+        }
+    } )
+};
+
+
+exports.editar_juego = (req,res)=>{
+    const id_juegos= req.body.id_juegos;
+    const titulo= req.body.titulo;
+    const imagen= req.body.imagen;
+    const descripcion= req.body.descripcion;
+    const status= req.body.status;
+    conexion.query('UPDATE tbljuegos SET ? WHERE id_juegos=?', [{ titulo:titulo,descripcion:descripcion, imagen:imagen,status:status,},id_juegos], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect('/');
+        }
+    })
+}
+
 
--- route.js
+++ route.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const conexion =  require('./conexion/conexion');
 
 
-/* router.get('/',(req, res)=>{
-    conexion.query('select * from tblusuarios', (error, results)=>{
+router.get('/',(req, res)=>{
+    conexion.query('select * from tbljuegos where status=1', (error, results)=>{
         if(error){
             throw error;    
         }else{
@@ -13,13 +13,14 @@ const conexion =  require('./conexion/conexion');
         }
     })
 })
- */
-router.get('/',(req, res)=>{
-    conexion.query('select * from tbljuegos', (error, results)=>{
+
+
+router.get('/mygames',(req, res)=>{
+    conexion.query('select * from tbljuegos where status=2', (error, results)=>{
         if(error){
             throw error;    
         }else{
-            res.render('index', {results:results});
+            res.render('mygames', {results:results});
         }
     })
 })
@@ -33,9 +34,40 @@ router.get('/login',(req, res)=>{
     res.render('login')
 })
 
+router.get('/mygames',(req, res)=>{
+    res.render('mygames')
+})
+
+router.get('/create_games',(req, res)=>{
+    res.render('create_games')
+})
+
+router.get('/edit/:id_juegos', (req,res)=>{
+    const id_juegos= req.params.id_juegos;
+    conexion.query('SELECT * FROM tbljuegos WHERE id_juegos=?',[id_juegos], (error, results)=>{
+        if(error){
+            throw error;    
+        }else{
+            res.render('edit', {juego:results[0]});
+        }
+    })
+})
+
+/*ELIMINAR*/ 
+router.get('/delete/:id_juegos', (req, res)=>{
+    const id_juegos= req.params.id_juegos; 
+    conexion.query('DELETE FROM tbljuegos WHERE id_juegos=?',[id_juegos], (error, results)=> {
+        if(error){
+            throw error;    
+        }else{
+            res.redirect('/');
+        }
+    })
+})
 
 const crud = require('./controllers/crud');
-router.post('/save', crud.save)
-
+router.post('/save', crud.save);
+router.post('/save_juego', crud.save_juego);
+router.post('/editar_juego', crud.editar_juego);
 
 module.exports = router;
